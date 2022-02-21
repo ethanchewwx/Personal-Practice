efb5d76bea57b927566b959cb6778e587621ec9d
ethanchewwx <100130816+ethanchewwx@users.noreply.github.com>
2022-02-21 23:14:57 +0800

    Delete Project Euler directory

--- Project Euler/Euler16.py
+++
@@ -1,7 +0,0 @@
-number = 2 ** 1000
-
-total = 0
-for digits in str(number):
-    total += int(digits)
-
-print(total)
--- Project Euler/Euler24.py
+++
@@ -1,8 +0,0 @@
-from itertools import permutations
-all_permutations_list = list(permutations(range(0, 10)))
-
-millionth_permutation_set = all_permutations_list[999999]
-millionth_permutation_set_str = [str(number) for number in millionth_permutation_set]
-millionth_permutation = "".join(millionth_permutation_set_str)
-print(millionth_permutation)
-
